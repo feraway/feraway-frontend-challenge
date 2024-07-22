2fb03c3056e4496e23025910f67f204c80451a1b
feraway <ferawayfromhere@gmail.com>
2024-07-22 11:28:44 -0300

    feat(): add buttons with basic operations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { useState } from "react";
 import { Combobox } from "@/components/ui/combobox";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SUPPORTED_CONTRACTS_SEPOLIA } from "@/lib/consts";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount, useReadContract } from "wagmi";
-import { formatUnits } from "viem";
+import { useAccount, useReadContract, useWriteContract } from "wagmi";
+import { formatUnits, parseUnits } from "viem";
 import { useStore } from "@/store";
 
 export default function Home() {
   const account = useAccount();
   const { targetAddress, setTargetAddress, contract, setContract } = useStore();
+  const { writeContract, status: writeContractStatus } = useWriteContract();
+  const [amount, setAmount] = useState();
 
   const selectedContract = contract
     ? SUPPORTED_CONTRACTS_SEPOLIA[
@@ -18,8 +21,16 @@ export default function Home() {
       ]
     : undefined;
 
+  const { data: balance } = useReadContract({
+    address: selectedContract?.address,
+    abi: selectedContract?.abi,
+    functionName: "balanceOf",
+    args: [account.address],
+    enabled: !!contract,
+  });
+
   const {
-    data: balance,
+    data: allowance,
     error,
     failureReason,
     fetchStatus,
@@ -28,10 +39,21 @@ export default function Home() {
   } = useReadContract({
     address: selectedContract?.address,
     abi: selectedContract?.abi,
-    functionName: "balanceOf",
-    args: [account.address],
-    enabled: !!contract,
+    functionName: "allowance",
+    args: [account.address, targetAddress],
+    enabled: !!contract && !!targetAddress,
   });
+
+  console.log("logger", {
+    allowance,
+    error,
+    failureReason,
+    fetchStatus,
+    isLoading,
+    status,
+  });
+
+  console.log("logger allowance", allowance);
 
   const comboboxOptions = Object.keys(SUPPORTED_CONTRACTS_SEPOLIA).map(
     (contract) => ({
@@ -58,13 +80,71 @@ export default function Home() {
         />
         <h2 className="text-2xl font-semibold mb-5">
           Balance:{" "}
-          {balance ? formatUnits(balance, selectedContract?.decimals) : "---"}
+          {balance
+            ? formatUnits(balance, selectedContract?.decimals ?? 0)
+            : "---"}
         </h2>
         <h2 className="text-2xl font-semibold mb-5">Target Address:</h2>
         <Input
           value={targetAddress}
           onChange={(e) => setTargetAddress(e.target.value)}
         />
+        <p className="my-5">
+          Allowance:{" "}
+          {allowance
+            ? formatUnits(allowance, selectedContract?.decimals ?? 0)
+            : "---"}
+        </p>
+        <h2 className="text-2xl font-semibold mb-5">Amount:</h2>
+        <Input value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <div className="my-5 w-96 flex justify-between">
+          <Button
+            onClick={() => {
+              writeContract({
+                address: selectedContract?.address,
+                abi: selectedContract?.abi,
+                functionName: "transfer",
+                args: [
+                  targetAddress,
+                  parseUnits(amount, selectedContract?.decimals),
+                ],
+              });
+            }}
+          >
+            Transfer
+          </Button>
+          <Button
+            onClick={() => {
+              writeContract({
+                address: selectedContract?.address,
+                abi: selectedContract?.abi,
+                functionName: "approve",
+                args: [
+                  targetAddress as Address,
+                  parseUnits(amount, selectedContract?.decimals),
+                ],
+              });
+            }}
+          >
+            Allowance
+          </Button>
+          <Button
+            onClick={() => {
+              writeContract({
+                address: selectedContract?.address,
+                abi: selectedContract?.abi,
+                functionName: "mint",
+                args: [
+                  account.address,
+                  parseUnits(amount, selectedContract?.decimals),
+                ],
+                enabled: !!contract,
+              });
+            }}
+          >
+            Mint
+          </Button>
+        </div>
       </main>
     </>
   );
