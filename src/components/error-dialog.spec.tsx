21cb5c6a34cae07955a16eb4473fa3efb5ed4b33
feraway <ferawayfromhere@gmail.com>
2024-08-01 09:32:20 -0300

    chore(): rename to spec

--- src/components/error-dialog.spec.tsx
+++ src/components/error-dialog.spec.tsx
@@ -2,9 +2,10 @@ import { render, screen } from "@testing-library/react";
 import { ErrorDialog } from "./error-dialog";
 import userEvent from "@testing-library/user-event";
 
+const description = "Error dialog test";
+
 describe("<ErrorDialog />", () => {
   test("It shouldn't render if open === false", () => {
-    const description = "Error dialog test";
     render(
       <ErrorDialog
         open={false}
@@ -14,22 +15,22 @@ describe("<ErrorDialog />", () => {
     );
     expect(screen.queryByText(description)).not.toBeInTheDocument();
   });
+
   test("It should render and display an error message if open === true", () => {
-    const description = "Error dialog test";
     render(
       <ErrorDialog open onConfirm={() => null} description={description} />
     );
     expect(screen.getByText(description)).toBeInTheDocument();
   });
+
   test("It should have an h2 spelling 'Error'", () => {
-    const description = "Error dialog test";
     render(
       <ErrorDialog open onConfirm={() => null} description={description} />
     );
     expect(screen.getByRole("heading")).toHaveTextContent("Error");
   });
+
   test("It should trigger the onConfirm fn after clicking the close button", async () => {
-    const description = "Error dialog test";
     const onConfirmFn = vi.fn();
     render(
       <ErrorDialog open onConfirm={onConfirmFn} description={description} />
