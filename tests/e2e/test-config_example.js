21f1766e17aee9c2f904f02f7ef212aa10f88a00
feraway <ferawayfromhere@gmail.com>
2024-08-07 10:26:48 -0300

    chore(): improve documentation for tests

---
+++ tests/e2e/test-config_example.js
@@ -0,0 +1,6 @@
+/**
+ * To enable e2e tests, create a "test-config.js" file with the following export and add
+ * a Target wallet address there, this will be the wallet to receive Transfers and Allowance
+ */
+
+export const TARGET_WALLET = "";
